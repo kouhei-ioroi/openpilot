74fdd2030de509ce0b3d15dc03d91ae46c3d1601
MoreTorque <104254025+MoreTore@users.noreply.github.com>
2024-08-26 07:16:02 -0500

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-export ATHENA_HOST="wss://connect-ws.duckdns.org"
-export API_HOST="https://connect-api.duckdns.org"
+export ATHENA_HOST="wss://athena.springerelectronics.com"
+export API_HOST="https://api.springerelectronics.com"
 export PASSIVE="0"
 exec ./launch_chffrplus.sh
 
